b74328621ae1354c3d80afdbbfb87d2d8b0f907f
Ashot Arutyunyan <jcl9595@gmail.com>
2017-01-10 15:46:39 -0500

    if else statement

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,6 @@
-
-
-function tylersiq(IQ) {
-	if (IQ== 100) {
-		console.log("<b> He is a smart</b>");
-	} else if (IQ > 100 && IQ < 350) {
-		console.log("<b> He is genious</b>")
-	} else {
-		console.log("<b> He is as smart as Trump")
-	}
+var age =11
+if (age<10) {
+	alert("this variable is less than 10")
+} else {
+	alert("the variable was" + age + " " + "you are too old")
 }
